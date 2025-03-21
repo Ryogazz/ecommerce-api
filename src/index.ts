6ca47e3ed9c62ccead1c9801665ae279f55fdd5b
josue <josuex.x.x@hotmail.com>
2025-03-20 21:34:47 -0300

    chore: add start script and tsc-watch for development

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! v2 - Node.js');
 });
+
+app.get("/users",(req: Request, res: Response) => {
+  let usuarios = [{
+    nome: "João",
+    idade: 30
+  },
+  {
+    nome: "Maria",
+    idade: 25
+  }]
+
+  res.send(usuarios);
+  
+})
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
