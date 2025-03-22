4f0a6db197d692283d372ded1cccefc5dbdf5043
josue <josuex.x.x@hotmail.com>
2025-03-22 16:20:30 -0300

    refactor: create UserRepository to centralize Firestore operations - Created UserRepository to handle all Firestore interactions - Moved Firestore logic from UserService to UserRepository - Updated UserService to use UserRepository for data access

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,53 @@
+import { CollectionReference, getFirestore } from 'firebase-admin/firestore';
+import { User } from '../models/user.model';
+
+export class UserRepository {
+
+  private collection: CollectionReference;
+
+  constructor() {
+    this.collection = getFirestore().collection('users');
+  }
+  async getAllUsers(): Promise<User[]> {
+    const snapshot = await  this.collection.get();
+    const users = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      }
+    }
+    ) as User[];
+    return users;
+  }
+
+  async getUserById(userId: string): Promise<User | null> {
+    const doc = await this.collection.doc(userId).get();
+    if (doc.exists) {
+      return {
+        id: doc.id,
+        ...doc.data()
+      } as User;
+    }
+    else {
+      return null;
+    }
+  }
+
+  async createUser(user: User): Promise<void> {
+    await this.collection.add(user);
+  }
+
+  async updateUser(user: User): Promise<void> {
+    const docRef = await this.collection.doc(user.id);
+    if ((await docRef.get()).exists) {
+      await docRef.set({
+        name: user.name,
+        email: user.email
+      });
+    }
+  }
+
+  async deleteUser(userId: string): Promise<void> {
+    await getFirestore().collection('users').doc(userId).delete();
+  }
+}
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,50 +1,43 @@
-import { getFirestore } from 'firebase-admin/firestore';
 import { User } from '../models/user.model';
 import { NotFoundError } from '../errors/not-found.erro';
+import { UserRepository } from '../repositories/user.repository';
 
 export class UserService {
+
+  private userRepository: UserRepository;
+
+  constructor() {
+    this.userRepository = new UserRepository();
+  }
   async getAllUsers(): Promise<User[]> {
-    const snapshot = await getFirestore().collection('users').get();
-    const users = snapshot.docs.map(doc => {
-      return {
-        id: doc.id,
-        ...doc.data()
-      }
-    }
-    ) as User[];
-    return users;
+    return await this.userRepository.getAllUsers();
+
   }
 
   async getUserById(userId: string): Promise<User> {
-    const doc = await getFirestore().collection('users').doc(userId).get();
-    if (doc.exists) {
-      return {
-        id: doc.id,
-        ...doc.data()
-      } as User;
-    }
-    else {
+    const user = await this.userRepository.getUserById(userId);
+    if (!user) {
       throw new NotFoundError('Usuário não encontrado');
     }
+    return user;
   }
 
   async createUser(user: User): Promise<void> {
-    await getFirestore().collection('users').add(user);
-}
+    return await this.userRepository.createUser(user);
+  }
 
   async updateUser(userId: string, user: User): Promise<void> {
-    const docRef = await getFirestore().collection('users').doc(userId);
-    if ((await docRef.get()).exists) {
-      await docRef.set({
-        name: user.name,
-        email: user.email
-      });
-    } else {
+    const _user = await this.userRepository.getUserById(userId);
+    if (!_user) {
       throw new NotFoundError('Usuário não encontrado');
     }
+
+    _user.name = user.name;
+    _user.email = user.email;
+      await this.userRepository.updateUser(_user);
   }
 
   async deleteUser(userId: string): Promise<void> {
-    await getFirestore().collection('users').doc(userId).delete();
+    return await this.userRepository.deleteUser(userId);
   }
 }
